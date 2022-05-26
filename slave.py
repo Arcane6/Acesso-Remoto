e51eb2b0abc5c4253909c8fc52ab48fe7db68edb
Antonio Gabriel <Devarcane6@gmail.com>
2022-05-25 21:17:44 -0300

    commit inicial

---
+++ slave.py
@@ -0,0 +1,20 @@
+import time
+import socket
+import sys
+import os
+
+from click import command
+
+s = socket.socket()
+host = "127.0.0.1"
+port = 8080
+s.connect((host,port))
+print("Conectado ao Servidor")
+
+command = s.recv(1024)
+command = command.decode()
+
+if command == "open":
+    print("Comando é: ", command)
+    s.send("Comando recebido".encode())
+    os.system('ls')
